ca91e9bd3e252da2d0e1a42f309aae45ee92a0af
Brad Kivell <Bradkivell@gmail.com>
2023-08-20 23:05:37 +1200

    setup cors for communication between front and backend

--- Week 4/backend/src/main.ts
+++ Week 4/backend/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ArticlesModule } from './articles/articles.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(ArticlesModule, {
     logger: ['error', 'warn', 'log', 'debug'],
   });
   await app.listen(3000);
+
+  app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }));
   
 }
 bootstrap();
--- Week 4/frontend/src/pages/articles/index.tsx
+++ Week 4/frontend/src/pages/articles/index.tsx
@@ -20,46 +20,45 @@ type ArticlesProps = {
 };
 
 const Articles: NextPage<ArticlesProps> = ({ articles }) => {
-    const headers: { key: keyof ArticlesInterface; label: string }[] = [
-      { key: "title", label: "Title" },
-      { key: "authors", label: "Authors" },
-      { key: "source", label: "Source" },
-      { key: "pubyear", label: "Publication Year" },
-      { key: "doi", label: "DOI" },
-      { key: "claim", label: "Claim" },
-      { key: "evidence", label: "Evidence" },
-    ];
-  
-    return (
-      <div className="container">
-        <h1>Articles Index Page</h1>
-        <p>Page containing a table of articles:</p>
-        <SortableTable headers={headers} data={articles} />
-      </div>
-    );
-  };
-  
-  export const getServerSideProps: GetServerSideProps<ArticlesProps> = async (_) => {
-    try {
-      // Make an API call to fetch data from the backend
-      const response = await axios.get(backendBaseUrl || "");
-      const articles = response.data;
-  
-      return {
-        props: {
-          articles,
-        },
-      };
-    } catch (error) {
-      console.error("Error fetching articles:", error);
-  
-      return {
-        props: {
-          articles: [],
-        },
-      };
-    }
-  };
-  
-  export default Articles;
-  
+  const headers: { key: keyof ArticlesInterface; label: string }[] = [
+    { key: "title", label: "Title" },
+    { key: "authors", label: "Authors" },
+    { key: "source", label: "Source" },
+    { key: "pubyear", label: "Publication Year" },
+    { key: "doi", label: "DOI" },
+    { key: "claim", label: "Claim" },
+    { key: "evidence", label: "Evidence" },
+  ];
+
+  return (
+    <div className="container">
+      <h1>Articles Index Page</h1>
+      <p>Page containing a table of articles:</p>
+      <SortableTable headers={headers} data={articles} />
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<ArticlesProps> = async (_) => {
+  try {
+    // Make an API call to fetch data from the backend
+    const response = await axios.get(backendBaseUrl || "", { withCredentials: true });
+    const articles = response.data;
+
+    return {
+      props: {
+        articles,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching articles:", error);
+
+    return {
+      props: {
+        articles: [],
+      },
+    };
+  }
+};
+
+export default Articles;
